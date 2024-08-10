a7b1c3b4608e6b965022d19e6984bd8a90415cfa
cherylcy <cherylngai23@gmail.com>
2024-08-10 09:22:23 -0700

    fix: movieId type is string

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -65,7 +65,7 @@ export default class ReviewsDAO {
 
   static async getReviewsByMovieId(movieId) {
     try {
-      const cursor = await reviews.find({ movieId: parseInt(movieId) });
+      const cursor = await reviews.find({ movieId: movieId });
       return cursor.toArray();
     } catch (e) {
       console.error(`Unable to get review: ${e}`);
